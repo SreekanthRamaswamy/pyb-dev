9bd897ecafefb8ed647bd1588c03c53058ccbc90
Sreekanth.Ramaswamy <sreekanth.ramaswamy@gmail.com>
2024-03-10 21:19:01 +0530

    Changed Helloworld.py code

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -21,5 +21,7 @@ out: Output stream to write the message to.
 out.write("Hello world of Python\n")
 
 # Call the function with a file object
+# Call the function with a file object
+
 
 helloworld(sys.stdout)
